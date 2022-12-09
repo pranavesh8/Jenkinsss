80105267d21738911ea001e698ddd5fd468d9ede
pranavesh8 <105205641+pranavesh8@users.noreply.github.com>
2022-12-09 17:43:40 +0530

    Update HotelManagementSystemApplicationTests.java

--- src/test/java/com/example/demo/HotelManagementSystemApplicationTests.java
+++ src/test/java/com/example/demo/HotelManagementSystemApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes={com.hotel.HotelManagementSystemApplication.class})
+//@SpringBootTest(classes={com.hotel.HotelManagementSystemApplication.class})
+@SpringBootTest(classes={com.hotel.controller.HotelController.javaclass})
+
 class HotelManagementSystemApplicationTests {
 
 	@Test
